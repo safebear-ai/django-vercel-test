aeb639ff19a181f2a039f48cfa8164e9d6f101fe
Louis Amon <louis@nuage.studio>
2023-12-19 18:10:21 +0100

    Serving static files via Django

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -118,9 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "public/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_ROOT = BASE_DIR / "public"
